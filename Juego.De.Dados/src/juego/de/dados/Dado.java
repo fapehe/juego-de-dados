62b10c8655f98e438d886dc5144eff332891332f
labing506 <labing506@labing50611.udistrital.edu.co>
2017-03-01 16:34:52 -0500

    juego de dados

---
+++ Juego.De.Dados/src/juego/de/dados/Dado.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego.de.dados;
+
+/**
+ *
+ * @author labing506
+ */
+public class Dado {
+    int numero;
+    
+}
